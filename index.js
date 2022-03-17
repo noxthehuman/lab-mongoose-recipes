2097688208715f5aa035b65f8c503fec218073dd
Nox <benaziz.ines@gmail.com>
2022-03-17 18:41:05 +0100

    iteration 6 not done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { updateOne } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,8 +17,37 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data)
+    // Recipe.create(
+    //   {
+    //     "title": "Asian Glazed Chicken Thighs",
+    //     "level": "Amateur Chef",
+    //     "ingredients": [
+    //       "1/2 cup rice vinegar",
+    //       "5 tablespoons honey",
+    //       "1/3 cup soy sauce (such as Silver Swan®)",
+    //       "1/4 cup Asian (toasted) sesame oil",
+    //       "3 tablespoons Asian chili garlic sauce",
+    //       "3 tablespoons minced garlic",
+    //       "salt to taste",
+    //       "8 skinless, boneless chicken thighs"
+    //     ],
+    //     "cuisine": "Asian",
+    //     "dishType": "main_course",
+    //     "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //     "duration": 40,
+    //     "creator": "Chef LePapu"
+    //   }
+    // )
+   //console.log()
+  const query = { name : /Rigatoni/ }
+  
+  Recipe.findOneAndUpdate(query, {duration : 100}, {option: 'after'})
+  Recipe.deleteOne({name : 'Carrot Cake'})
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  }
+    
+);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+    default: 'Easy Peasy'
+  },
+  ingredients: [ String ],
+  cusine: String,
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+    default: 'breakfst'
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    minimum: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
